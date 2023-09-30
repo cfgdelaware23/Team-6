4f81144d6619c11ef09dac95253950e7ad0dd5cc
Dylan Tom <dt425@cornell.edu>
2023-09-29 21:56:43 -0400

    parsing data file

--- acb/db/ingest_data.py
+++ acb/db/ingest_data.py
@@ -5,21 +5,24 @@ availability_file_path = "./availability.csv"
 hosting_file_path = "./hosting.csv"
 
 # connect to mysql instance
-conn = pymysql.connect(host='localhost',
-                       user='root',
-                       password='password',
-                       db='my_database')
+# conn = pymysql.connect(host='localhost',
+#                        user='root',
+#                        password='password',
+#                        db='my_database')
 
-    cursor = conn.cursor()
+# cursor = conn.cursor()
 
-with open(availability_file_path, newline='') as csvfile:
+with open(hosting_file_path, newline='', encoding='utf-8-sig') as csvfile:
     test = csv.reader(csvfile, delimiter=',')
-    for row in test:
-        cursor.execute('INSERT INTO testcsv(names, \
-            classes, mark )' \
-            'VALUES("%s", "%s", "%s")', 
-            row)
+    header = next(test)
+    print(header)
+    # for row in test:
+    #     print(row)
+#         cursor.execute('INSERT INTO acb_data(names, \
+#             classes, mark )' \
+#             'VALUES("%s", "%s", "%s")', 
+#             row)
 
 
-cursor.close()
+# cursor.close()
 
