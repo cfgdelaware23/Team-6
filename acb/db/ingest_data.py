23a0952345770c800c275bcf1efcca77bd1e47fb
Dylan Tom <dt425@cornell.edu>
2023-09-29 21:47:29 -0400

    added support for sql connection to ingest data in python

--- acb/db/ingest_data.py
+++ acb/db/ingest_data.py
@@ -1,9 +1,25 @@
 import csv
+import pymysql
 
 availability_file_path = "./availability.csv"
 hosting_file_path = "./hosting.csv"
 
+# connect to mysql instance
+conn = pymysql.connect(host='localhost',
+                       user='root',
+                       password='password',
+                       db='my_database')
+
+    cursor = conn.cursor()
+
 with open(availability_file_path, newline='') as csvfile:
-    test = csv.reader(csvfile, delimiter=' ')
-    for line in test:
-        print(line)
+    test = csv.reader(csvfile, delimiter=',')
+    for row in test:
+        cursor.execute('INSERT INTO testcsv(names, \
+            classes, mark )' \
+            'VALUES("%s", "%s", "%s")', 
+            row)
+
+
+cursor.close()
+
