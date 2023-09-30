3d373c7a7adb355248ec5676777eff0a4f181eea
Sakhee Desai <sakheed@udel.edu>
2023-09-29 20:29:18 -0400

    stashing

--- acb/src/app/page.js
+++ acb/src/app/page.js
@@ -3,6 +3,10 @@ import Image from "/public/logo.jpeg"
 import styles from './page.module.css'
 import { useState } from "react";
 import { Users } from './Users';
+import { Volunteer } from './Volunteer';
+import { Admin } from './Admin';
+import { EventCoordinator } from './EventCoordinator';
+
 import { Heading, Radio, RadioGroup, Stack, HStack, Img} from "@chakra-ui/react";
 import { ViewIcon } from "@chakra-ui/icons";
 
@@ -47,7 +51,6 @@ export default function App() {
             return (<></>)
           }
           }
-    }
   return (
     <div className = "App">
       <HStack p={6} bg="blue.100"justify="space-between">
@@ -78,93 +81,3 @@ export default function App() {
   );
 }
 
-
-  //   <main className={styles.main}>
-  //     <div className={styles.description}>
-  //       <p>
-  //         Get started by editing&nbsp;
-  //         <code className={styles.code}>src/app/page.js</code>
-  //       </p>
-  //       <div>
-  //         <a
-  //           href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-  //           target="_blank"
-  //           rel="noopener noreferrer"
-  //         >
-  //           By{' '}
-  //           <Image
-  //             src="/vercel.svg"
-  //             alt="Vercel Logo"
-  //             className={styles.vercelLogo}
-  //             width={100}
-  //             height={24}
-  //             priority
-  //           />
-  //         </a>
-  //       </div>
-  //     </div>
-
-  //     <div className={styles.center}>
-  //       <Image
-  //         className={styles.logo}
-  //         src="/next.svg"
-  //         alt="Next.js Logo"
-  //         width={180}
-  //         height={37}
-  //         priority
-  //       />
-  //     </div>
-
-  //     <div className={styles.grid}>
-  //       <a
-  //         href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-  //         className={styles.card}
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         <h2>
-  //           Docs <span>-&gt;</span>
-  //         </h2>
-  //         <p>Find in-depth information about Next.js features and API.</p>
-  //       </a>
-
-  //       <a
-  //         href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-  //         className={styles.card}
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         <h2>
-  //           Learn <span>-&gt;</span>
-  //         </h2>
-  //         <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-  //       </a>
-
-  //       <a
-  //         href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-  //         className={styles.card}
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         <h2>
-  //           Templates <span>-&gt;</span>
-  //         </h2>
-  //         <p>Explore the Next.js 13 playground.</p>
-  //       </a>
-
-  //       <a
-  //         href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-  //         className={styles.card}
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         <h2>
-  //           Deploy <span>-&gt;</span>
-  //         </h2>
-  //         <p>
-  //           Instantly deploy your Next.js site to a shareable URL with Vercel.
-  //         </p>
-  //       </a>
-  //     </div>
-  //   </main>
-  // )
