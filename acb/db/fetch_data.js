ce5856d9bfd28a2db80fcc6dbfa331bc03e95d12
Dylan Tom <dt425@cornell.edu>
2023-09-30 02:39:13 -0400

    Added buttons routing to form population

---
+++ acb/db/fetch_data.js
@@ -0,0 +1,19 @@
+// import { createConnection } from 'mysql';
+
+// const conn = createConnection({
+//     host: "database6.cs0tgohxdki8.us-east-2.rds.amazonaws.com",
+//     user: "admin",
+//     password: "",
+//     database: "database6"
+// });
+
+// export function fetchAvailabilities() {
+//     conn.connect(function (err) {
+//         if (err) throw err;
+//         console.log("Connected!");
+//         conn.query("SELECT * from availabilities", function (err, result) {
+//             if (err) throw err;
+//             console.log("Result: " + result);
+//         });
+//     });
+// }
--- acb/src/app/page.js
+++ acb/src/app/page.js
@@ -2,18 +2,19 @@
 import Image from "/public/logo.jpeg"
 import styles from './page.module.css'
 import Events from "../../components/Events";
-
+import Link from "next/link";
 
 import { useState } from "react";
 import { Users } from './Users';
 import { Admin } from './Admin';
-import VolunteerForm  from "../../components/VolunteerForm";
+import VolunteerForm from "../../components/VolunteerForm";
 import EventOrganizerForm from "../../components/EventOrganizerForm";
-import EventForm  from "../../components/EventForm";
+import EventForm from "../../components/EventForm";
 
-import { Heading, Radio, RadioGroup, Stack, HStack, Img} from "@chakra-ui/react";
+import { Heading, Radio, RadioGroup, Stack, HStack, Img } from "@chakra-ui/react";
 import { EventCoordinator } from './EventCoordinator';
 import { ViewIcon } from "@chakra-ui/icons";
+
 
 export default function App() {
   const [selectedRole, setRole] = useState(localStorage.getItem("role") || "Volunteer");
@@ -21,6 +22,8 @@ export default function App() {
 
   const test = ["September 26th", "September 29th", "October 1st"]
   const test2 = ["Bingo Night", "Weekly Book Club", "Annual Friendsgiving Celebration", "Just a Chill Gathering", "Let's Talk About Life"]
+
+  // fetchAvailabilities();
 
   // function selectUser() {
   //   if (selectedRole == "User") {
@@ -30,77 +33,78 @@ export default function App() {
   //   }
   // }
 
-    // function volunteer(){
-    //   if (selectedRole == "Volunteer"){
-    //     return (
-    //       <Events title="Upcoming Events" table={test2}/>,
-    //       <VolunteerForm></VolunteerForm>
-    //       );
-    //   }
-    //   else{
-    //     return (<></>)
-    //   }
-    //   }
+  // function volunteer(){
+  //   if (selectedRole == "Volunteer"){
+  //     return (
+  //       <Events title="Upcoming Events" table={test2}/>,
+  //       <VolunteerForm></VolunteerForm>
+  //       );
+  //   }
+  //   else{
+  //     return (<></>)
+  //   }
+  //   }
 
-      function event_coordinator(){
-        if (selectedRole == "Event Coordinator"){
-          return (<EventForm></EventForm>);
-        }
-        else{
-          return (<></>)
-        }
-        }
+  function event_coordinator() {
+    if (selectedRole == "Event Coordinator") {
+      return (<EventForm></EventForm>);
+    }
+    else {
+      return (<></>)
+    }
+  }
 
-        function admin(){
-          if (selectedRole == "Admin"){
-            return (<Admin></Admin>);
-          }
-          else{
-            return (<></>)
-          }
-          }
+  function admin() {
+    if (selectedRole == "Admin") {
+      return (<Admin></Admin>);
+    }
+    else {
+      return (<></>)
+    }
+  }
   console.log(selectedRole)
 
   return (
-    <div className = "App">
-      <HStack p={6} bg="#362B7C"justify="space-between">
-                <HStack>
-                    <Img src="./logo.jpeg" alt="ACB Logo" height="100px"/>
-                    <Heading size="xl" pl={2} color="#FFFFFF">American Council of the Blind </Heading>
-                </HStack>
-                <Stack>
-                    <Heading as='h3' size="md" color="#FFFFFF"><ViewIcon/>&nbsp;&nbsp;Viewing as: {selectedRole}</Heading>
-                    <RadioGroup color="#FFFFFF" onChange={role => {
-                        localStorage.setItem("role", role);
-                        setRole(role);
-                        if (!users.includes(role)){
-                            setUsers([...users, role]);
-                        };
-                    }} value={selectedRole}>
-                        <Stack direction='row'>
-                            {users.map((p) =>
-                                <Radio key={p} borderColor="black" colorScheme='red' pr={4} value={p}>{p}</Radio>
-                            )}
-                        </Stack>
-                    </RadioGroup>
-                </Stack>
-            </HStack>
-            <br/>
-                    <div>
-                        {/* <VolunteerForm></VolunteerForm> */}
-                        
-                        {selectedRole==="Event Coordinator"?<Events title="What's New!" table={test}/> : <></>}
-                        {selectedRole==="Event Coordinator"?<EventForm></EventForm> : <></>}  
-               
-                        {selectedRole==="Volunteer"?<Events title="Upcoming Events" table={test2}/> : <></>} 
-                        {selectedRole==="Volunteer"?<VolunteerForm></VolunteerForm> : <></>}
-                        {/* {event_coordinator()}
+    <div className="App">
+      <HStack p={6} bg="#362B7C" justify="space-between">
+        <HStack>
+          <Img src="./logo.jpeg" alt="ACB Logo" height="100px" />
+          <Heading size="xl" pl={2} color="#FFFFFF">American Council of the Blind </Heading>
+        </HStack>
+        <Stack>
+          <Heading as='h3' size="md" color="#FFFFFF"><ViewIcon />&nbsp;&nbsp;Viewing as: {selectedRole}</Heading>
+          <RadioGroup color="#FFFFFF" onChange={role => {
+            localStorage.setItem("role", role);
+            setRole(role);
+            if (!users.includes(role)) {
+              setUsers([...users, role]);
+            };
+          }} value={selectedRole}>
+            <Stack direction='row'>
+              {users.map((p) =>
+                <Radio key={p} borderColor="black" colorScheme='red' pr={4} value={p}>{p}</Radio>
+              )}
+            </Stack>
+          </RadioGroup>
+        </Stack>
+      </HStack>
+      <br />
+      <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+
+        {/* <VolunteerForm></VolunteerForm> */}
+
+        {selectedRole === "Event Coordinator" ? <Events title="What's New!" table={test} /> : <></>}
+        {selectedRole === "Event Coordinator" ? <Link href="/eventOrganizer"><button style={{ backgroundColor: '#36287C', color: 'white', border: 'none', padding: '8px 16px', borderRadius: '4px', cursor: 'pointer' }}>Add New Event</button></Link> : <></>}
+        {/* {selectedRole === "Event Coordinator" ? <EventForm></EventForm> : <></>} */}
+
+        {selectedRole === "Volunteer" ? <Events title="Upcoming Events" table={test2} /> : <></>}
+        {selectedRole === "Volunteer" ? <Link href="/volunteer_form"><button style={{ backgroundColor: '#36287C', color: 'white', border: 'none', padding: '8px 16px', borderRadius: '4px', cursor: 'pointer' }}>Add New Hours</button></Link> : <></>}
+        {/* {event_coordinator()}
                         {admin()} */}
-                    </div>
-                    <div>
-                        {/* {selectUser()} */}
-                    </div>  
       </div>
+      <div>
+      </div>
+    </div>
 
   );
 }
