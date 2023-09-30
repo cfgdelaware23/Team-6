25999d8fd5fc1bd71102790b286b7177f237db2a
HPatel0 <hari.patel0225@gmail.com>
2023-09-29 21:31:18 -0400

    new event organizer style

--- acb/components/eventOrganizerForm.jsx
+++ acb/components/eventOrganizerForm.jsx
@@ -1,15 +1,53 @@
-// import Form from "../components/Form"
-"use client"
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
+const FormContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
 
-const eventOrganizerForm = () => {
-  const [lastName, setLastName] = useState('')
-  const [firstName, setFirstName] = useState('')
-  const [email, setEmail] = useState('')
-  const [company, setCompany] = useState('')
-  const [date, setDate] = useState('')
-  const [recurrence, setRecurrence] = useState('')
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+  background-color: #fff;
+  max-width: 400px;
+  width: 100%;
+`;
+
+const StyledInput = styled.input`
+  width: 100%;
+  padding: 10px;
+  margin: 5px 0;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  outline: none;
+`;
+
+const StyledButton = styled.button`
+  padding: 10px 20px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 16px;
+  margin-top: 10px;
+`;
+
+const EventOrganizerForm = () => {
+  const [lastName, setLastName] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [email, setEmail] = useState('');
+  const [company, setCompany] = useState('');
+  const [date, setDate] = useState('');
+  const [recurrence, setRecurrence] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -18,85 +56,51 @@ const eventOrganizerForm = () => {
   };
 
   return (
-    <div>
-      <h1>Event Organizer Form</h1>
-      <form onSubmit={handleSubmit}>
-        <div style={{margin: '10px'}}>
-          <label htmlFor="lastName">lastName:</label>
-          <input
-            type="text"
-            id="lastName"
-            name="lastName"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-            required
-          />
-        </div>
-        <div style={{margin: '10px'}}>
-          <label htmlFor="firstName">firstName:</label>
-          <input
-            type="text"
-            id="firstName"
-            name="firstName"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-            required
-          />
-        </div>
-        
-        <div style={{margin: '10px'}}>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-
-
-        <div style={{margin: '10px'}}>
-          <label htmlFor="text">Company:</label>
-          <input
-            type="text"
-            id="company"
-            name="company"
-            value={company}
-            onChange={(e) => setCompany(e.target.value)}
-          />
-        </div>
-
-        <div style={{margin: '10px'}}>
-          <label htmlFor="date">Date:</label>
-          <input
-            type="date"
-            id="date"
-            name="date"
-            value={date}
-            onChange={(e) => setDate(e.target.value)}
-          />
-        </div>
-
-        <div style={{margin: '10px'}}>
-          <label htmlFor="text">Recurrence:</label>
-          <input
-            type="text"
-            id="recurrence"
-            name="recurrence"
-            value={recurrence}
-            onChange={(e) => setRecurrence(e.target.value)}
-          />
-        </div>
-
-
-        <div>
-          <button type="submit">Submit</button>
-        </div>
-      </form>
-    </div>
+    <FormContainer>
+      <StyledForm onSubmit={handleSubmit}>
+        <StyledInput
+          type="text"
+          placeholder="Last Name"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+          required
+        />
+        <StyledInput
+          type="text"
+          placeholder="First Name"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+          required
+        />
+        <StyledInput
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <StyledInput
+          type="text"
+          placeholder="Company"
+          value={company}
+          onChange={(e) => setCompany(e.target.value)}
+        />
+        <StyledInput
+          type="date"
+          placeholder="Date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+        />
+        <StyledInput
+          type="text"
+          placeholder="Recurrence"
+          value={recurrence}
+          onChange={(e) => setRecurrence(e.target.value)}
+        />
+        <StyledButton type="submit">Submit</StyledButton>
+      </StyledForm>
+    </FormContainer>
   );
 };
 
-export default eventOrganizerForm;
+export default EventOrganizerForm;
