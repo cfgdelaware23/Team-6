1dd4d7f1e3673beaa4d1ee65f88c1ac693891e96
Dylan Tom <dt425@cornell.edu>
2023-09-30 00:35:54 -0400

    insert sql query

--- acb/db/ingest_data.py
+++ acb/db/ingest_data.py
@@ -5,24 +5,33 @@ availability_file_path = "./availability.csv"
 hosting_file_path = "./hosting.csv"
 
 # connect to mysql instance
-# conn = pymysql.connect(host='localhost',
-#                        user='root',
-#                        password='password',
-#                        db='my_database')
+conn = pymysql.connect(host='database6.cs0tgohxdki8.us-east-2.rds.amazonaws.com',
+                       user='admin',
+                       password='',
+                       db="database6")
 
-# cursor = conn.cursor()
+print(conn)
+
+cursor = conn.cursor()
+
+# def createTable():
+#     sqlQuery            = "CREATE TABLE acb_data(id int, Day varchar(32), Time varchar(32), Title varchar(32), Account varchar(32), Host varchar(32), Moderator varchar(32), Facilitator varchar(32), Streamer varchar(32), Broadcaster varchar(32))"   
+#     cursor.execute(sqlQuery)
+
+# createTable()
 
 with open(hosting_file_path, newline='', encoding='utf-8-sig') as csvfile:
     test = csv.reader(csvfile, delimiter=',')
     header = next(test)
+    # fields: 'Day', 'Time', 'Title', 'Account', 'Host', 'Moderator', 'Facilitator', 'Streamer', 'Broadcaster'
     print(header)
-    # for row in test:
-    #     print(row)
-#         cursor.execute('INSERT INTO acb_data(names, \
-#             classes, mark )' \
-#             'VALUES("%s", "%s", "%s")', 
-#             row)
+    for row in test:
+        print(row)
+        cursor.execute('INSERT INTO vTable(Day1, \
+            Time1, Titles, Account, Host, moderator, facilitator, Streamer, Broadcaster )' \
+            'VALUES("%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s")', 
+            [row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8]])
 
 
-# cursor.close()
+cursor.close()
 
