105a0c584e1a975d80751188d9444c5f55b129be
dstokic <67245788+dstokic@users.noreply.github.com>
2023-09-29 22:56:45 -0400

    Implemented basic event display functionality

--- acb/src/app/page.js
+++ acb/src/app/page.js
@@ -1,16 +1,20 @@
 "use client"
 import Image from "/public/logo.jpeg"
 import styles from './page.module.css'
+import Events from "../../components/Events";
+
 
 import { useState } from "react";
 import { Users } from './Users';
 import { Heading, Radio, RadioGroup, Stack, HStack, Img } from "@chakra-ui/react";
 import { ViewIcon } from "@chakra-ui/icons";
 
-
 export default function App() {
   const [selectedRole, setRole] = useState(localStorage.getItem("role") || "User");
   const [users, setUsers] = useState(["Volunteer", "Event Coordinator", "Admin"]);
+
+  const test = ["September 26th", "September 29th", "October 1st"]
+  const test2 = ["Bingo Night", "Weekly Book Club", "Annual Friendsgiving Celebration", "Just a Chill Gathering", "Let's Talk About Life"]
 
   function selectUser() {
     if (selectedRole == "User") {
@@ -49,7 +53,7 @@ export default function App() {
 
   return (
     <div className="App">
-      <HStack p={6} bg="blue.100" justify="space-between">
+      <HStack p={6} bg="blue.100" justify="space-between" className="App-header">
         <HStack>
           <Img src="./logo.jpeg" alt="ACB Logo" height="100px" />
           <Heading size="xl" pl={2} color="#FFFFFF">American Council of the Blind </Heading>
@@ -72,6 +76,8 @@ export default function App() {
         </Stack>
       </HStack>
       <br />
+      <Events title="What's New!" table={test}/>
+      <Events title="Upcoming Events" table={test2}/>
     </div>
 
   );
