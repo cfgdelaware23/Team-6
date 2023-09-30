fdd7bfc2b2244b1e4d13c1ab275b98a93b5b4cf9
Dylan Tom <dt425@cornell.edu>
2023-09-30 06:44:11 -0400

    fixed admin button placement

--- acb/components/Admin.jsx
+++ acb/components/Admin.jsx
@@ -84,7 +84,7 @@ const Admin = () => {
                 </tbody>
                 </table>
             </div>
-            <div className="btn-container">
+            <div className="btn-container" style={{ marginTop: '100px', textAlign: 'center' }}>
                 <Button className="download-btn">
                 <CSVLink className="csv-download" filename="volunteer_data" data={csvData}>Download Me</CSVLink>
                 </Button>
